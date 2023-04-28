32569c4ff733237cebcd99ef6ae9fbc179476195
FluffaTheDuck <98738425+FluffaTheDuck@users.noreply.github.com>
2023-04-28 14:49:35 +0200

    started creating client side

--- WeekRecapJoining/client/src/App.tsx
+++ WeekRecapJoining/client/src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useState } from 'react'
+import { useQuery, gql } from '@apollo/client';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [persons, setPersons] = useState([]);
+  const getPersons = gql`
+    query {
+      people {
+        id
+        name
+        age
+      }
+    }
+  `;
+
+  const displayPersons = () => {
+    const { loading, error, data } = useQuery(getPersons);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+    setPersons(data.people);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>People</h1>
+        {persons && persons.map((person: any) => (
+          <div key={person.id}>
+            <p>{person.name}</p>
+            <p>{person.age}</p>
+          </div>
+        ))}
+        <button onClick={displayPersons}>Get People</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
