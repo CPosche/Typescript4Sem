00c60e2aa677358cbe3cefb70346c499ddc4b1a0
FluffaTheDuck <cbp@posche.dk>
2023-05-02 21:08:40 +0200

    apolloclient with lazyusequery

--- WeekRecapJoining/client/src/App.tsx
+++ WeekRecapJoining/client/src/App.tsx
@@ -1,39 +1,55 @@
-
-import './App.css'
-import { useState } from 'react'
-import { useQuery, gql } from '@apollo/client';
+import "./App.css";
+import { useLazyQuery, gql } from "@apollo/client";
 
 function App() {
-  const [persons, setPersons] = useState([]);
   const getPersons = gql`
     query {
       people {
         id
         name
         age
+        email
       }
     }
   `;
 
   const displayPersons = () => {
-    const { loading, error, data } = useQuery(getPersons);
+    const [getPeople, { loading, error, data }] = useLazyQuery(getPersons);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
-    setPersons(data.people);
-  }
+
+    return (
+      <div>
+        <button onClick={() => getPeople()}>Get People</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.people.map((person: any) => (
+                <tr key={person.id}>
+                  <td>{person.name}</td>
+                  <td>{person.age}</td>
+                  <td>{person.email}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
   return (
-      <div className="App">
-        <h1>People</h1>
-        {persons && persons.map((person: any) => (
-          <div key={person.id}>
-            <p>{person.name}</p>
-            <p>{person.age}</p>
-          </div>
-        ))}
-        <button onClick={displayPersons}>Get People</button>
-      </div>
-  )
+    <div className="App">
+      <h1>People</h1>
+      {displayPersons()}
+    </div>
+  );
 }
 
-export default App
+export default App;
